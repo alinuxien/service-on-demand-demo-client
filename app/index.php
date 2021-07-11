d459a17b848ddf1a585181de6c6428917bc45098
Ali Akrour <ali.akrour@gmail.com>
2021-07-11 16:34:41 +0000

    test end to end : php code modification on deployed env

--- app/index.php
+++ app/index.php
@@ -18,6 +18,6 @@ if ($conn->connect_error) {
 // Affichage du résultat de la connexion et des infos sur le serveur de BDD
 echo "Connexion reussie a la BDD\n<br />";
 echo "Version du serveur de BDD : ".mysqli_get_server_info($conn)."\n<br />";
-echo "Ce test fonctionne bien\n<br />";
+echo "Et celui ci\n<br />";
 ?>
 
